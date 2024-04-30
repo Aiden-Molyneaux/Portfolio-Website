f337024ab55d510824ccfbfd94b6351b4cd1bfff
Aiden Molyneaux <aiden@wicket.io>
2024-04-30 13:28:54 -0400

    Adjust mobile home page

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import heartSVG from '../assets/heart.svg';
 
 export default function Home() {
-  window.localStorage.setItem('location', JSON.stringify({ path: '/home' }));
-  
   return (
     <>
       <div className='homeContent'>
         <h1>AIDEN MOLYNEAUX</h1>
         <h2>PASSIONATE PROGRAMMER & FULL-STACK DEVELOPER</h2>
-        <p className='topMargin'>MY HEART BEATS TO PROGRAM</p>
+        <p className='topMargin'>CODING MAKES MY HEART BEAT</p>
       </div>
       <div className='heartContainer'>
         <img src={heartSVG} alt='A red geometric heart drawing, animated to be beating' className='heartSVG'/>
