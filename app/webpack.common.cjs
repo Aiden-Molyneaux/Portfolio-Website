eb65d581dda695a69ba53125d075f5c9bfaa989c
Aiden Molyneaux <aiden@wicket.io>
2024-04-30 09:07:00 -0400

    Fix favicon and site refresh issue

--- app/webpack.common.cjs
+++ app/webpack.common.cjs
@@ -9,7 +9,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
       filename: 'index.html',
-      inject: 'body'
+      inject: 'body',
+      favicon: './src/assets/headshot.png'
     })
   ],
   module: {
@@ -40,7 +41,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', 'dist'),
     clean: true,
   },
 };
