825581430edb3de6801fd7ca0bd1aedf1186a1ad
Aiden Molyneaux <aiden@wicket.io>
2024-10-02 13:53:58 -0400

    Attempt fix #2

--- app/src/pages/Contact.jsx
+++ app/src/pages/Contact.jsx
@@ -50,7 +50,7 @@ export default function Contact() {
       const response = await fetch('https://u863rlss5k.execute-api.us-east-1.amazonaws.com/Attempt3/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ body: emailData }),
+        body: JSON.stringify({ "body" : emailData }),
       });
       const result = await response.json();
       console.log(result);
